b9e8e0e76108da02ae46ca27467234c41d29cd89
강예린 <yerin7786@gmail.com>
2025-11-03 19:21:19 +0900

    feat(Application): Application 구현
    프로그램 시작을 위해 Application을 구현한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.lotto.LottoNumbersGenerator;
+import lotto.view.Input;
+import lotto.view.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Output output = new Output();
+        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
+        LottoController lottoController = new LottoController(input, output, lottoNumbersGenerator);
+        lottoController.play();
     }
 }
