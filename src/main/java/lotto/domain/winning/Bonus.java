d50f18ebc56871e96b6a03543ca3780b9656d28c
강예린 <yerin7786@gmail.com>
2025-11-03 17:25:15 +0900

    feat(Input): 보너스 번호 범위 검증 기능 구현
    보너스 번호가 범위를 벗어나지 않는지 검증하고,
    벗어날 경우 메시지와 함께 예외를 발생시킨다.

--- src/main/java/lotto/domain/winning/Bonus.java
+++ src/main/java/lotto/domain/winning/Bonus.java
@@ -1,13 +1,18 @@
 package lotto.domain.winning;
 
+import static lotto.domain.LottoRuleConstant.MAX_LOTTO_NUMBER;
+import static lotto.domain.LottoRuleConstant.MIN_LOTTO_NUMBER;
+
 public class Bonus {
 
     private static final String BONUS_NUMBER_NOT_NUMBER = "[ERROR] 보너스 번호는 숫자(정수) 하나만 입력할 수 있습니다.";
+    private static final String BONUS_NUMBER_OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     private final int number;
 
     public Bonus(String inputNumber) {
         int number = parseBonusNumber(inputNumber);
+        validateBonusNumberRange(number);
         this.number = number;
     }
 
@@ -16,6 +21,12 @@ public class Bonus {
             return Integer.parseInt(number);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(BONUS_NUMBER_NOT_NUMBER);
+        }
+    }
+
+    private void validateBonusNumberRange(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(BONUS_NUMBER_OUT_OF_RANGE);
         }
     }
 }
