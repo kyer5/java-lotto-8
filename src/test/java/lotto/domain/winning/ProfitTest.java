8c455a4d0d8bb250e4125791cf63558e95685d3a
강예린 <yerin7786@gmail.com>
2025-11-03 18:36:54 +0900

    test(Profit): 당첨 결과에 따른 총 당첨 금액 계산 실패 테스트
    당첨 결과에 따른 총 당첨 금액을 계산하는 테스트를 추가한다.

---
+++ src/test/java/lotto/domain/winning/ProfitTest.java
@@ -0,0 +1,30 @@
+package lotto.domain.winning;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import lotto.domain.lotto.PurchaseAmount;
+import lotto.domain.winning.value.Rank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProfitTest  {
+
+    @DisplayName("당첨 결과에 따라 총 당첨 금액을 계산한다.")
+    @Test
+    void 당첨_결과에_따라_총_당첨_금액을_계산한다() {
+        // given
+        Map<Rank, Integer> result = Map.of(
+                Rank.FIRST, 1,
+                Rank.THIRD, 2,
+                Rank.FIFTH, 3
+        );
+        PurchaseAmount purchaseAmount = new PurchaseAmount("10000");
+
+        // when
+        Profit profit = new Profit(purchaseAmount, result);
+
+        // then
+        assertThat(profit).extracting("totalWinningAmount").isEqualTo(2_003_015_000L);
+    }
+}
