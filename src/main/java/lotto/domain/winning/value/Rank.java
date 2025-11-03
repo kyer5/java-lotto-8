621243b07b29d5dd726fca213e4620979323262f
강예린 <yerin7786@gmail.com>
2025-11-03 17:46:42 +0900

    feat(Rank): 당첨 기준 정의
    각 1등부터 5등까지의 기준을 enum으로 정의한다.

---
+++ src/main/java/lotto/domain/winning/value/Rank.java
@@ -0,0 +1,21 @@
+package lotto.domain.winning.value;
+
+public enum Rank {
+
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean isMatchBonusNumber;
+    private final int winningAmount;
+
+    Rank(int matchCount, boolean isMatchBonusNumber, int winningAmount) {
+        this.matchCount = matchCount;
+        this.isMatchBonusNumber = isMatchBonusNumber;
+        this.winningAmount = winningAmount;
+    }
+}
