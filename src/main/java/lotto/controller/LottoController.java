e396974345dde61c72c47b6f7f7c8567c04fac47
강예린 <yerin7786@gmail.com>
2025-11-03 19:16:50 +0900

    feat(LottoController): 로또 발급 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,9 @@
 package lotto.controller;
 
 import java.util.function.Supplier;
+import lotto.domain.lotto.LottoIssuer;
 import lotto.domain.lotto.LottoNumbersGenerator;
+import lotto.domain.lotto.LottoTicket;
 import lotto.domain.lotto.PurchaseAmount;
 import lotto.view.Input;
 import lotto.view.Output;
@@ -20,6 +22,8 @@ public class LottoController {
 
     public void play() {
         PurchaseAmount purchaseAmount = purchase();
+        LottoTicket lottoTicket = issue(purchaseAmount);
+        output.printIssuedLottos(lottoTicket);
     }
 
     private PurchaseAmount purchase() {
@@ -27,6 +31,11 @@ public class LottoController {
             String inputPurchaseAmount = input.readPurchaseAmount();
             return new PurchaseAmount(inputPurchaseAmount);
         });
+    }
+
+    private LottoTicket issue(PurchaseAmount purchaseAmount) {
+        LottoIssuer lottoIssuer = new LottoIssuer(purchaseAmount, lottoNumbersGenerator);
+        return lottoIssuer.generateLottoTicket();
     }
 
     private <T> T retryUntilValid(Supplier<T> supplier) {
