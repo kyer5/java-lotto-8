12ee2b8423295c3f34269a22bd8da31b88b81b18
강예린 <yerin7786@gmail.com>
2025-11-03 17:06:39 +0900

    feat(Input): 보너스 번호 입력 기능 구현
    사용자로부터 보너스 번호를 입력받는다.

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -9,6 +9,7 @@ public class Input {
     private static final String DELIMITER = ",";
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String WINNING_NUMBERS_INVALID_FORMAT = "[ERROR] 허용되지 않은 문자가 포함되어 있습니다. (옳은 입력 예시: 1,2,3,4,5,6)";
 
     public String readPurchaseAmount() {
@@ -19,6 +20,11 @@ public class Input {
     public List<Integer> readWinningNumbers() {
         System.out.println("\n" + WINNING_NUMBERS_INPUT_MESSAGE);
         return parseToInteger(Console.readLine());
+    }
+
+    public String readBonusNumber() {
+        System.out.println("\n" + BONUS_NUMBER_INPUT_MESSAGE);
+        return Console.readLine();
     }
 
     private List<Integer> parseToInteger(String inputWinningNumbers) {
