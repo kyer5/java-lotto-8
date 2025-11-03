d4a0f0eae0c3b9a84de3e5c04de34bfdfb886a25
강예린 <yerin7786@gmail.com>
2025-11-03 17:03:02 +0900

    feat(Lotto): 로또 번호 숫자 범위 검증 기능 구현
    각 로또 번호가 범위를 벗어나는지 검증하고,
    벗어날 경우 메시지와 함께 예외를 발생시킨다.

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto.domain.lotto;
 
 import static lotto.domain.LottoRuleConstant.LOTTO_NUMBER_COUNT;
+import static lotto.domain.LottoRuleConstant.MAX_LOTTO_NUMBER;
+import static lotto.domain.LottoRuleConstant.MIN_LOTTO_NUMBER;
 
 import java.util.List;
 
 public class Lotto {
 
     private static final String LOTTO_NUMBER_INVALID_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String LOTTO_NUMBER_OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     private final List<Integer> numbers;
 
@@ -17,11 +20,21 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
+        validateRange(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(LOTTO_NUMBER_INVALID_COUNT);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        boolean isInvalid = numbers.stream()
+                .anyMatch(number -> number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER);
+
+        if (isInvalid) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_OUT_OF_RANGE);
         }
     }
 }
